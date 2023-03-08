e91f1db8bd15495d0d5b1fe0a5ba75e967693caf
volcas <nnair25@gmail.com>
2023-03-08 14:51:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,251 +1,218 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-
-import requests, os
-from gwpy.timeseries import TimeSeries
-from gwosc.locate import get_urls
-from gwosc import datasets
-from gwosc.api import fetch_event_json
-
-from copy import deepcopy
+# from fbprophet import Prophet
+# from fbprophet.diagnostics import performance_metrics
+# from fbprophet.diagnostics import cross_validation
+# from fbprophet.plot import plot_cross_validation_metric
 import base64
+import pickle
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+import datetime
+from sklearn.metrics import mean_squared_error
+import plotly.express as px
+st.title('Time Series Forecasting Using Streamlit')
 
-from helper import make_audio_file
+st.write("IMPORT DATA")
+st.write("Import the time series csv file. It should have two columns labelled as 'ds' and 'y'.The 'ds' column should be of datetime format  by Pandas. The 'y' column must be numeric representing the measurement to be forecasted.")
 
-# Use the non-interactive Agg backend, which is recommended as a
-# thread-safe backend.
-# See https://matplotlib.org/3.3.2/faq/howto_faq.html#working-with-threads.
-import matplotlib as mpl
-mpl.use("agg")
-
-##############################################################################
-# Workaround for the limited multi-threading support in matplotlib.
-# Per the docs, we will avoid using `matplotlib.pyplot` for figures:
-# https://matplotlib.org/3.3.2/faq/howto_faq.html#how-to-use-matplotlib-in-a-web-application-server.
-# Moreover, we will guard all operations on the figure instances by the
-# class-level lock in the Agg backend.
-##############################################################################
-from matplotlib.backends.backend_agg import RendererAgg
-_lock = RendererAgg.lock
+data = st.file_uploader('Upload first file here',type='csv')
+# data = pd.read_csv('dataset_for_static_model.csv')
 
 
-# -- Set page config
-apptitle = 'GW Quickview'
+data2 = st.file_uploader('Upload second file here',type='csv')
 
-st.set_page_config(page_title=apptitle, page_icon=":eyeglasses:")
 
-# -- Default detector list
-detectorlist = ['H1','L1', 'V1']
 
-# Title the app
-st.title('Gravitational Wave Quickview')
+def model_run(appdata_main,select_region,select_team1,select_team2):
+    appdata=appdata_main[appdata_main['Region']==select_region]
+#         appdata=appdata[(appdata['matchName'].str.contains(select_team1)) & (appdata['matchName'].str.contains(select_team2))]    
+    df_cat = pd.concat([pd.DataFrame(typeOfDay_cat_encoder.transform(appdata[['typeOfDay']]), columns=typeOfDay_cat_encoder.get_feature_names_out(),
+                index = appdata.index),
+                pd.DataFrame(Festival_cat_encoder.transform(appdata[['Festival']]), columns=Festival_cat_encoder.get_feature_names_out(),
+                index = appdata.index), 
+                pd.DataFrame(inning_cat_encoder.transform(appdata[['inning']]), columns=inning_cat_encoder.get_feature_names_out(),
+                index = appdata.index),
+                pd.DataFrame(region_cat_encoder.transform(appdata[['Region']]), columns=region_cat_encoder.get_feature_names_out(),
+                index = appdata.index),
+                pd.DataFrame(timeOfDay_cat_encoder.transform(appdata[['timeOfDay']]), columns=timeOfDay_cat_encoder.get_feature_names_out(),
+                index = appdata.index)], axis=1)
 
-st.markdown("""
- * Use the menu at left to select data and set plot parameters
- * Your plots will appear below
-""")
 
-@st.cache(ttl=3600, max_entries=10)   #-- Magic command to cache data
-def load_gw(t0, detector, fs=4096):
-    strain = TimeSeries.fetch_open_data(detector, t0-14, t0+14, sample_rate = fs, cache=False)
-    return strain
+    df_num = appdata[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore','Pred',col]]
+    X = pd.concat([df_num.iloc[:,:-1], df_cat], axis=1)
+    y = pd.DataFrame(df_num.loc[:,col])
+    offset=len(X)-periods_input
+    X_train = X.loc[0:offset-1,:]
+    X_test = X.loc[offset:,:]
 
-@st.cache(ttl=3600, max_entries=10)   #-- Magic command to cache data
-def get_eventlist():
-    allevents = datasets.find_datasets(type='events')
-    eventset = set()
-    for ev in allevents:
-        name = fetch_event_json(ev)['events'][ev]['commonName']
-        if name[0:2] == 'GW':
-            eventset.add(name)
-    eventlist = list(eventset)
-    eventlist.sort()
-    return eventlist
+    y_train = y.loc[0:offset-1,:]
+    y_test = y.loc[offset:,:]
+
+    feature_scaler = StandardScaler()
+    X_train[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore']] = feature_scaler.fit_transform(X_train[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore']])
+
+    target_scaler = StandardScaler()
+    y_train = target_scaler.fit_transform(y_train)
+
+    X_test[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore']] = feature_scaler.transform(X_test[['Balls', 'team1Fanbase', 'team2Fanbase', 'AvgFirstInningsScore']])
+    y_test = target_scaler.transform(y_test)
+
+#     svr = SVR()
+
+    # train the model on training data
+#     svr.fit(X_train.fillna(0), y_train)
+
+    # make predictions on test data
+    y_pred = svr.predict(X_test.fillna(0))
+    # y_pred=1.33*y_pred
+    # calculate mean squared error
+#     mse = mean_squared_error(y_test, y_pred)
+    y_pred1=svr.predict(X_train.fillna(0))
+    # mse = mean_squared_error(y_train, y_pred1)
+    # print(mse)
+    # print mean squared error
+
+    total_pred=np.concatenate([y_pred1, y_pred])
+    total=np.concatenate([y_train, y_test])
+#     mse = mean_squared_error(total, total_pred)
+
+#     # print mean squared error
+#     print("Mean squared error: ", mse)
+#     df['Pred_'+col]=target_scaler.inverse_transform(total_pred.reshape(-1, 1))                     
+#     print("For the col ",col," mean squared error: ", mse)
+
+
+#     forecast = fcst[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+
+#     forecast_filtered =  forecast[forecast['ds'] > max_date]    
+#     st.write(total_pred)
+
+
+#     st.write("The next visual shows the actual (red line) and predicted (blue line) values over time.") 
+    total_pred=target_scaler.inverse_transform(total_pred.reshape(-1, 1))
+    total=target_scaler.inverse_transform(total.reshape(-1, 1))
+    return total_pred,total
+
+
+if data is not None and data2 is not None:
+    df_new = pd.read_csv(data)
+    extra=pd.read_csv(data2)
+    appdata_main=extra.merge(df_new, on=['Datetime','inning','matchName','timeOfDay'],how='left',suffixes=('', '_y'))
+
+
+    appdata_main['Datetime'] = appdata_main['Datetime'].apply(lambda x: datetime.datetime.strftime(datetime.datetime.strptime(x, "%d-%m-%Y %H:%M"), "%Y-%m-%d %H:%M"))    
+#     st.write(data)
+
+    max_date = appdata_main['Datetime'].max()
+
+
+    select_tg = st.sidebar.selectbox('What TG Level?',
+                                    ['2-12_male','2-12_female','13-21_male','13-21_female',
+                                     '22-30_male','22-30_female','31-40_male','31-40_female',
+                                     '41-50_male','41-50_female','51-60_male','51-60_female',
+                                     '61+_male','61+_female'])    
+
+
+
+    with open('model_'+select_tg+'.pkl', 'rb') as f:
+        svr = pickle.load(f)
+
+    team_list1=['CSK', 'MI', 'RCB', 'LSG', 'RR', 'KKR', 'PBKS', 'GT', 'DC', 'SRH']    
+    select_team1 = st.sidebar.selectbox('Select Team 1',
+                                    team_list1)    
+    team_list2=['CSK', 'MI', 'RCB', 'LSG', 'RR', 'KKR', 'PBKS', 'GT', 'DC', 'SRH']    
+    team_list2.remove(select_team1)
+    select_team2 = st.sidebar.selectbox('Select Team 2',
+                                    team_list2)   
+
+    region_list=['AP / Telangana', 'Assam / North East / Sikkim', 'Bihar/Jharkhand',
+           'Delhi', 'Guj / D&D / DNH', 'Har/HP/J&K', 'Karnataka', 'Kerala',
+           'MP/Chhattisgarh', 'Mah / Goa', 'Odisha', 'Pun/Cha', 'Rajasthan',
+           'TN/Pondicherry', 'UP/Uttarakhand', 'West Bengal']
+
+    select_region= st.sidebar.selectbox('Select Region',
+                                    region_list)
+
+
+
+    periods_input = st.number_input('How many days forecast do you want?',
+    min_value = 1, max_value = 365)  
+#     select_region = st.text_input('Which Region?')  
+
+    st.write("VISUALIZE FORECASTED DATA")
+    st.write("The following plot shows future predicted values. 'yhat' is the predicted value; upper and lower limits are 80% confidence intervals by default")
+
+
+
+
+
+# if data is not None:
+    col=select_tg
+#     offset = df[df['Datetime']<'2022-05-21'].shape[0]   ## for train test split
+
+    appdata=appdata_main.copy()
+    typeOfDay_cat = appdata[['typeOfDay']]
+
+    typeOfDay_cat_encoder = OneHotEncoder(sparse=False)
+    typeOfDay_cat_1hot = typeOfDay_cat_encoder.fit_transform(typeOfDay_cat)
+
+    Festival_cat = appdata[['Festival']]
+
+    Festival_cat_encoder = OneHotEncoder(sparse=False)
+    Festival_cat_1hot = Festival_cat_encoder.fit_transform(Festival_cat)
+
+    inning_cat = appdata[['inning']]
+
+    inning_cat_encoder = OneHotEncoder(sparse=False)
+    inning_cat_1hot = inning_cat_encoder.fit_transform(inning_cat)
+
+    region_cat = appdata[['Region']]
+
+    region_cat_encoder = OneHotEncoder(sparse=False)
+    region_cat_1hot = region_cat_encoder.fit_transform(region_cat)
+
+    timeOfDay_cat = appdata[['timeOfDay']]
+
+    timeOfDay_cat_encoder = OneHotEncoder(sparse=False)
+    timeOfDay_cat_1hot = timeOfDay_cat_encoder.fit_transform(timeOfDay_cat)
     
-st.sidebar.markdown("## Select Data Time and Detector")
+    total_pred,total=model_run(appdata,select_region,select_team1,select_team2)
+     
+    new_new=appdata[appdata['Region']=select_region]
+    new_new['pred']=total_pred
+    new_new['new_total']=total
 
-# -- Get list of events
-eventlist = get_eventlist()
-
-#-- Set time by GPS or event
-select_event = st.sidebar.selectbox('How do you want to find data?',
-                                    ['By event name', 'By GPS'])
-
-if select_event == 'By GPS':
-    # -- Set a GPS time:        
-    str_t0 = st.sidebar.text_input('GPS Time', '1126259462.4')    # -- GW150914
-    t0 = float(str_t0)
-
-    st.sidebar.markdown("""
-    Example times in the H1 detector:
-    * 1126259462.4    (GW150914) 
-    * 1187008882.4    (GW170817) 
-    * 1128667463.0    (hardware injection)
-    * 1132401286.33   (Koi Fish Glitch) 
-    """)
-
-else:
-    chosen_event = st.sidebar.selectbox('Select Event', eventlist)
-    t0 = datasets.event_gps(chosen_event)
-    detectorlist = list(datasets.event_detectors(chosen_event))
-    detectorlist.sort()
-    st.subheader(chosen_event)
-    st.write('GPS:', t0)
-    
-    # -- Experiment to display masses
-    try:
-        jsoninfo = fetch_event_json(chosen_event)
-        for name, nameinfo in jsoninfo['events'].items():        
-            st.write('Mass 1:', nameinfo['mass_1_source'], 'M$_{\odot}$')
-            st.write('Mass 2:', nameinfo['mass_2_source'], 'M$_{\odot}$')
-            st.write('Network SNR:', int(nameinfo['network_matched_filter_snr']))
-            eventurl = 'https://gw-osc.org/eventapi/html/event/{}'.format(chosen_event)
-            st.markdown('Event page: {}'.format(eventurl))
-            st.write('\n')
-    except:
-        pass
-
-    
-#-- Choose detector as H1, L1, or V1
-detector = st.sidebar.selectbox('Detector', detectorlist)
-
-# -- Select for high sample rate data
-fs = 4096
-maxband = 2000
-high_fs = st.sidebar.checkbox('Full sample rate data')
-if high_fs:
-    fs = 16384
-    maxband = 8000
+    figure1 =px.line(
+        data_frame =new_new,
+                x = new_new['Datetime'],
+            #     x = test_set['Datetime'].astype(str),
+            #         y=["rat%_Universe scaled", "rr_reqRR_ratio", "maxSR scaled"],
+                y=["new_total","pred"],
+            color_discrete_sequence=['red', "blue"])
+            #     symbol=sample_df['timeOfDay'],
+            #     symbol_sequence=['circle-open', 'square'],
+            #         y = 'rat%_Universe',
+            #     opacity = 0.9,
+            #     orientation = "v",
+            #     barmode = 'group',
+            #     text="match_name"
 
 
-# -- Create sidebar for plot controls
-st.sidebar.markdown('## Set Plot Parameters')
-dtboth = st.sidebar.slider('Time Range (seconds)', 0.1, 8.0, 1.0)  # min, max, default
-dt = dtboth / 2.0
+            # fig2.update_traces(textposition=sample_df['textPosition'])
 
-st.sidebar.markdown('#### Whitened and band-passed data')
-whiten = st.sidebar.checkbox('Whiten?', value=True)
-freqrange = st.sidebar.slider('Band-pass frequency range (Hz)', min_value=10, max_value=maxband, value=(30,400))
+            #     fig2.add_scatter(x=sample_df['Start Time'], y=sample_df['RR scaled'], name="run rate")
+
+            #     fig2.add_trace(go.Table(cells={"values":df.T.values}, header={"values":df.columns}), row=1,col=1)
 
 
-# -- Create sidebar for Q-transform controls
-st.sidebar.markdown('#### Q-tranform plot')
-vmax = st.sidebar.slider('Colorbar Max Energy', 10, 500, 25)  # min, max, default
-qcenter = st.sidebar.slider('Q-value', 5, 120, 5)  # min, max, default
-qrange = (int(qcenter*0.8), int(qcenter*1.2))
+            # fig2.update_xaxes(tickangle=290)
+    figure1.update_layout(showlegend=True,font=dict(family="Courier New",size=12,color='Black'),
+                                   title=f"SVR with linear kernel model prediction",
+                                   xaxis_title="Time of day",
+                                   yaxis_title="Predicted Viewership",
+                                   width=1000,height=500)
 
-#-- Create a text element and let the reader know the data is loading.
-strain_load_state = st.text('Loading data...this may take a minute')
-try:
-    strain_data = load_gw(t0, detector, fs)
-except:
-    st.warning('{0} data are not available for time {1}.  Please try a different time and detector pair.'.format(detector, t0))
-    st.stop()
-    
-strain_load_state.text('Loading data...done!')
-
-#-- Make a time series plot
-
-cropstart = t0-0.2
-cropend   = t0+0.1
-
-cropstart = t0 - dt
-cropend   = t0 + dt
-
-st.subheader('Raw data')
-center = int(t0)
-strain = deepcopy(strain_data)
-
-with _lock:
-    fig1 = strain.crop(cropstart, cropend).plot()
-    #fig1 = cropped.plot()
-    st.pyplot(fig1, clear_figure=True)
-
-
-# -- Try whitened and band-passed plot
-# -- Whiten and bandpass data
-st.subheader('Whitened and Band-passed Data')
-
-if whiten:
-    white_data = strain.whiten()
-    bp_data = white_data.bandpass(freqrange[0], freqrange[1])
-else:
-    bp_data = strain.bandpass(freqrange[0], freqrange[1])
-
-bp_cropped = bp_data.crop(cropstart, cropend)
-
-with _lock:
-    fig3 = bp_cropped.plot()
-    st.pyplot(fig3, clear_figure=True)
-
-# -- Allow data download
-download = {'Time':bp_cropped.times, 'Strain':bp_cropped.value}
-df = pd.DataFrame(download)
-csv = df.to_csv(index=False)
-b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-fn =  detector + '-STRAIN' + '-' + str(int(cropstart)) + '-' + str(int(cropend-cropstart)) + '.csv'
-href = f'<a href="data:file/csv;base64,{b64}" download="{fn}">Download Data as CSV File</a>'
-st.markdown(href, unsafe_allow_html=True)
-
-# -- Make audio file
-st.audio(make_audio_file(bp_cropped), format='audio/wav')
-
-# -- Notes on whitening
-with st.expander("See notes"):
-    st.markdown("""
- * Whitening is a process that re-weights a signal, so that all frequency bins have a nearly equal amount of noise. 
- * A band-pass filter uses both a low frequency cutoff and a high frequency cutoff, and only passes signals in the frequency band between these values.
-
-See also:
- * [Signal Processing Tutorial](https://share.streamlit.io/jkanner/streamlit-audio/main/app.py)
-""")
-
-
-st.subheader('Q-transform')
-
-hq = strain.q_transform(outseg=(t0-dt, t0+dt), qrange=qrange)
-
-with _lock:
-    fig4 = hq.plot()
-    ax = fig4.gca()
-    fig4.colorbar(label="Normalised energy", vmax=vmax, vmin=0)
-    ax.grid(False)
-    ax.set_yscale('log')
-    ax.set_ylim(bottom=15)
-    st.pyplot(fig4, clear_figure=True)
-
-
-with st.expander("See notes"):
-
-    st.markdown("""
-A Q-transform plot shows how a signal’s frequency changes with time.
-
- * The x-axis shows time
- * The y-axis shows frequency
-
-The color scale shows the amount of “energy” or “signal power” in each time-frequency pixel.
-
-A parameter called “Q” refers to the quality factor.  A higher quality factor corresponds to a larger number of cycles in each time-frequency pixel.  
-
-For gravitational-wave signals, binary black holes are most clear with lower Q values (Q = 5-20), where binary neutron star mergers work better with higher Q values (Q = 80 - 120).
-
-See also:
-
- * [GWpy q-transform](https://gwpy.github.io/docs/stable/examples/timeseries/qscan.html)
- * [Reading Time-frequency plots](https://labcit.ligo.caltech.edu/~jkanner/aapt/web/math.html#tfplot)
- * [Shourov Chatterji PhD Thesis](https://dspace.mit.edu/handle/1721.1/34388)
-""")
-
-
-st.subheader("About this app")
-st.markdown("""
-This app displays data from LIGO, Virgo, and GEO downloaded from
-the Gravitational Wave Open Science Center at https://gw-openscience.org .
-
-
-You can see how this works in the [Quickview Jupyter Notebook](https://github.com/losc-tutorial/quickview) or 
-[see the code](https://github.com/jkanner/streamlit-dataview).
-
-""")
+    st.write(figure1)
